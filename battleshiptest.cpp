8d3e3b353d9650d71ea0f24dbdde66c598c8cea5
Luiz Filipe Mariz <lfsmariz@gmail.com>
2018-09-15 11:43:40 -0300

    recapitulando

--- battleshiptest.cpp
+++ battleshiptest.cpp
@@ -4,39 +4,45 @@ using namespace std;
 
 struct puzzle
 {
-bool revelado; // Variável que irá testar se o usuário já viu o espaço
-char posicao; // Variável vai indicar o conteudo da posição  
+	bool diponibilidade; // Variável que irá testar se espaço está disponível
+	char posicao; // Variável vai indicar o conteudo da posição  
 }
 
 puzzle geramatriz (int tamanho)
 {// Função geradora da matriz
-puzzle tabuleiro[][]; int t2;
+	short t2 = tamanho
+	puzzle tabuleiro[t2][t2]; 
+	
 
-if (t>10 && t<15)
-{
- 	for(int i=0; i < t; i++)
- 	{
-  	 for(int j=0; j < t; i++)
-  	 {
- 	   tabuleiro[i][j]="-"; // o hífen representa água 
-  	 }	
- 	}
- return tabuleiro [t][t];
+	if (t>7 && t<15)
+	{
+ 		for(int i=0; i < t; i++)
+ 		{
+			for(int j=0; j < t; i++)
+  	 		{
+ 			tabuleiro[i][j].posicao="-"; // o hífen representa água 
+			tabuleiro[i][j].disponibilidade = true; // espaço alocado como disponível
+  	 		}	
+ 		}
+
+ 	return tabuleiro;
+	}
+
+	else 
+	{	
+	cout << "Tamanho inválido, insira outro tamanho para o tabuleiro" << endl; 
+	cin >> t2;
+	return gerametriz (t2);	
+	}
 }
 
-else 
-{	
-cout << "Tamanho inválido, insira outro tamanho para o tabuleiro" << endl; 
-cin >> t2;
-return gerametriz (t2);	
-}
-}
+
 
 int main()
 {
-int t; // Tamanho da matriz
-cout << "Insira o tamanho do tabuleiro" << endl;
-cin >> t; 
-geramatriz (t);
-// TEM QUE RECEBER A MATRIZ AINDA
+	int t; // Tamanho da matriz
+	cout << "Insira o tamanho do tabuleiro" << endl;
+	cin >> t; 
+	geramatriz (t);
+	// TEM QUE RECEBER A MATRIZ AINDA
 }
