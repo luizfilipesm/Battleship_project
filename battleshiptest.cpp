6647800b5960bef51dccbd60286590d10dede1f2
Luiz Filipe Mariz <lfsmariz@gmail.com>
2018-09-15 15:33:18 -0300

    tabuleiro gerado

--- battleshiptest.cpp
+++ battleshiptest.cpp
@@ -1,66 +1,81 @@
 #include<iostream>
-
-using namespace std;
+#include<stdlib.h>
+#include <ctime>
+#include <cstdlib>
+#include <string>
+#include <vector>
 
 struct board
 {
-	puzzle[tam][tam]
+    puzzle** tab;
 }
-
 struct puzzle
 {
-	bool diponibilidade; // Variável que irá testar se espaço está disponível
-	char conteudo; // Variável vai indicar o conteudo da posição  
-}
+    bool avalability;
+    char content;    
 
-void preenchematriz (tabuleiro[], q_tabuleiros)
+};
+
+puzzle** board_gen(int tam)
 {
-	
-}
+    puzzle** tab_aqua = 0;
+    tab_aqua = new puzzle *[tam];
+    
+    for(int h = 0; h < tam; h++)
+        {
+            tab_aqua[h] = new puzzle [tam];
+        }
+    
+    for(int i=0; i < tam; i++)
+    {
+        for(int j=0; j < tam; j++ )
+        {
+            tab_aqua[i][j].avalability = true;
+            tab_aqua[i][j].content = '-';
+        }
 
-puzzle geramatriz (int tamanho)
-{// Função geradora da matriz
-	short t2 = tamanho
-	puzzle tabuleiro[t2][t2]; 
-	
+    }
 
-	if (t>7 && t<15)
-	{
- 		for(int i=0; i < t; i++)
- 		{
-			for(int j=0; j < t; i++)
-  	 		{
- 			tabuleiro[i][j].posicao="-"; // o hífen representa água 
-			tabuleiro[i][j].disponibilidade = true; // espaço alocado como disponível
-  	 		}	
- 		}
-
- 	return tabuleiro;
-	}
-
-	else 
-	{	
-	cout << "Tamanho inválido, insira outro tamanho para o tabuleiro" << endl; 
-	cin >> t2;
-	return gerametriz (t2);	
-	}
+    return tab_aqua;
 }
 
 
-
-int main()
+int main(int argc, char const *argv[])
 {
-	int tamanho; // Tamanho da matriz
-	int q_tabuleiros // Quantidade de tabuleiros
-	board tabs[q_tabuleiros];
-	
-	// apenas visual
-	cout << "Insira o tamanho do tabuleiro" << endl;
-	cin >> t;
+    char *p;
+    int tamanho_tabuleiro, numero_tabuleiros;
+    tamanho_tabuleiro = strtol(argv[1], &p, 10);
+    numero_tabuleiros = strtol(argv[2], &p, 10);
+    
+    
 
-	for(int i=0 ; i < q_tabuleiros; i++)
-		tabs.puzzle[i] = geramatriz (tamanho);
-		preenchematriz(tab.puzzle[i])
-	
-	// TEM QUE RECEBER A MATRIZ AINDA
+    for (int boards = 0; boards < numero_tabuleiros; boards++){
+    puzzle** tabuleiro;
+    tabuleiro = board_gen(tamanho_tabuleiro);
+
+    for(int i=0; i < tamanho_tabuleiro; i++)
+    {
+        std::cout <<""<<std::endl;
+        for(int j=0; j < tamanho_tabuleiro; j++ )
+        {
+           std::cout << tabuleiro[i][j].content ;
+        }
+
+    }
+
+    /*for ( int b = 0; b < tamanho_tabuleiro; b++ )
+        {
+        delete [] tabuleiro[b];   
+        }
+        delete [] tabuleiro;
+        tabuleiro = 0;
+        
+
+    /*std::cout << tamanho_tabuleiro << "\n";
+    std::cout << numero_tabuleiros << "\n";
+    std::cout << argc <<"\n";
+    */
+
+    return 0;
 }
+
