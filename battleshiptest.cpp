be65355f87f8ccc840a11eace7b723370801f774
Gabrielle <gabrielleduartee@outlook.com>
2018-09-15 18:41:40 -0300

    Update battleshiptest.cpp

--- battleshiptest.cpp
+++ battleshiptest.cpp
@@ -8,49 +8,48 @@
 
 
 struct puzzle
-{
-    bool avalability;
-    char content;    
-
-};
+{// Estrutura do conteúdo da matriz
+    bool avalability; // Variável que denota a disponibilidade da célula
+    char content; // Variável que denota o conteúdo da célula
+}; 
 
 struct board
-{
+{// Estturtura do tabuleiro
     puzzle** tab;
 };
 
 puzzle** board_gen(int tam)
 {
-    puzzle** tab_aqua = 0;
-    tab_aqua = new puzzle *[tam];
+    puzzle** tab_aqua = 0; // Matriz receberá '0' inicializando o tabuleiro
+    tab_aqua = new puzzle *[tam]; //
     
     for(int h = 0; h < tam; h++)
         {
-            tab_aqua[h] = new puzzle [tam];
+            tab_aqua[h] = new puzzle [tam]; // 
         }
     
     for(int i=0; i < tam; i++)
     {
         for(int j=0; j < tam; j++ )
         {
-            tab_aqua[i][j].avalability = true;
-            tab_aqua[i][j].content = '-';
+            tab_aqua[i][j].avalability = true;// Preenchendo a tabela de disponibilidade, deixando todos os espaços disponíveis
+            tab_aqua[i][j].content = '-'; // Preenchendo todos os espaços com "-" que representa água 
         }
 
     }
 
-    return tab_aqua;
+    return tab_aqua; // Retorna a tabela inicializada e preenchida 
 }
 
 
 int main(int argc, char const *argv[])
 {
-    char *p;
+    char *p; 
     int tamanho_tabuleiro, numero_tabuleiros;
     tamanho_tabuleiro = strtol(argv[1], &p, 10);
     numero_tabuleiros = strtol(argv[2], &p, 10);
     
-     board tabuleiro[numero_tabuleiros];
+     board tabuleiro[numero_tabuleiros];todo
 
     for (int b = 0; b < numero_tabuleiros; b++)
 	{
